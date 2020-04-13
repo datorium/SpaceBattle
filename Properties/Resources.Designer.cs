68631b022fc4e574c1f58e4bbe091914189f6cee
Datorium <hello@datorium.lv>
2020-04-13 17:07:15 +0300

    version 17:07

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpaceBattle.Properties
-{
-
-
+namespace SpaceBattle.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,124 @@ namespace SpaceBattle.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpaceBattle.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_off_1 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_off_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_off_2 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_off_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_off_3 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_off_3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_off_4 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_off_4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_on_1 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_on_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_on_2 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_on_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_on_3 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_on_3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rocket_on_4 {
+            get {
+                object obj = ResourceManager.GetObject("rocket_on_4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
--- Spaceship.cs
+++ Spaceship.cs
@@ -10,16 +10,36 @@ namespace SpaceBattle
 {
     class Spaceship : PictureBox
     {
+        private int imageCount = 1;
+        private string engineStatus = "off";
+        private Timer timerAnimate;
+        
         public Spaceship()
         {
             InitializeSpaceship();
+            InitializeTimerAnimate();
         }
 
         private void InitializeSpaceship()
         {
-            this.BackColor = Color.Orange;
-            this.Height = 40;
-            this.Width = 100;
+            this.BackColor = Color.Transparent;
+            this.Height = 80;
+            this.Width = 40;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Image = Properties.Resources.rocket_on_1;
+        }
+
+        private void InitializeTimerAnimate()
+        {
+            timerAnimate = new Timer();
+            timerAnimate.Tick += new EventHandler(TimerAnimate_Tick);
+            timerAnimate.Interval = 100;
+            timerAnimate.Start();
+        }
+
+        private void TimerAnimate_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
